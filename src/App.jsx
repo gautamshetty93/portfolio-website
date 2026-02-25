da2748e54eabce8104c135956228a0aba4861a4c
gautamshetty93 <gautam.shetty3@gmail.com>
2026-02-25 20:10:47 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+import {useEffect, useState} from 'react'
+import Navbar from "./components/Navbar";
+import AOS from 'aos'
+import 'aos/dist/aos.css';
+
+const App = () => {
+    const [darkMode, setDarkMode] = useState(true)
+
+    useEffect(() => {
+        AOS.init({
+            duration: 1000,
+            once: false,
+            offset : 100
+        });
+        document.documentElement.classList.add('dark');
+    }, []);
+
+    const toggleDarkMode = () => {
+        const newDarkMode = !darkMode;
+        setDarkMode(newDarkMode);
+        document.documentElement.classList.toggle('dark');
+    };
+
+    return (
+        <div className={
+            darkMode
+                ? 'bg-linear-to-br from-gray-900 via-[#0d182e] to-gray-900 min-h-screen'
+                : 'bg-linear-to-br from-gray-50 to-blue-50 min-h-screen'
+        }>
+            <Navbar darkMode={darkMode} toggleDarkMode={toggleDarkMode} />
+        </div>
+)
+}
+export default App
