9a5b57822ba05daf344cc7777902abbe807e059e
gautamshetty93 <gautam.shetty3@gmail.com>
2026-02-27 20:06:17 +0530

    Adding About and skills

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import Navbar from "./components/Navbar";
 import AOS from 'aos'
 import 'aos/dist/aos.css';
 import Hero from "./components/Hero.jsx";
+import About from "./components/About.jsx";
+import Skills from "./components/Skills.jsx";
 
 const App = () => {
     const [darkMode, setDarkMode] = useState(true)
@@ -34,6 +36,8 @@ const App = () => {
         }>
             <Navbar darkMode={darkMode} toggleDarkMode={toggleDarkMode} />
             <Hero darkMode={darkMode} />
+            <About darkMode={darkMode} />
+            <Skills darkMode={darkMode} />
         </div>
 )
 }
